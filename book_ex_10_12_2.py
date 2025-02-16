55f4211e598ad5dde4959a2ea56d9d2778e2c926
L Preudhomme <codedroots.github@gmail.com>
2025-02-15 23:35:56 -0500

    Update book_ex_10_12_2.py
    removed useless comments

--- book_ex_10_12_2.py
+++ book_ex_10_12_2.py
@@ -1,10 +1,5 @@
-# code cannot run as is as it does not have a file in reference
 # Write a program to read through the mbox-short.txt and figure 
 # out the distribution by hour of the day for each of the messages.
-# You can pull the hour out from the 'From ' line by finding the 
-# time and then splitting the string a second time using a colon.
-# Once you have accumulated the counts for each hour, print out 
-# the counts, sorted by hour as shown below.
 
 fname = input("Enter file:")
 if len(fname) < 1:
